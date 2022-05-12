433f0b1d161c8c849060ad7c5e4ab9fb3c0e0af4
mig.ramos10@gmail.com <mig.ramos10@gmail.com>
2022-05-11 23:24:25 -0300

    tarefa usando inquirer e chalk + validacao

--- 2_fundamentos/13_sync_async/assincrono.js
+++ 2_fundamentos/13_sync_async/assincrono.js
@@ -2,7 +2,7 @@ const fs =  require('fs')
 console.log("início")
 
 fs.writeFile('arquivo.txt', 'oi', function (err) {
-    setTimeout(function () {
+    setTimeout(() => {
         console.log('Arquivo criado!')
     }, 2000)
 })
---
+++ 2_fundamentos/15_tarefa_02/index.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+inquirer.prompt([
+    {name: 'nome', message: 'Qual o seu nome? '},
+    {name: 'idade', message: 'Qual a sua idade? '}
+])
+.then((answers) => {
+
+    if(!answers.nome || !answers.idade){
+        throw new Error('O nome e a idade são obrigatórios!')
+    }
+    const response = `O nome do usuáio é ${answers.nome} e ele tem ${answers.idade} anos!`
+
+    console.log(chalk.bgYellow.black(response))
+})
+.catch((err) => console.log(err))
