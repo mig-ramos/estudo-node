b7f8b24ce535064ce8cab4d37da049632ed52159
mig.ramos10@gmail.com <mig.ramos10@gmail.com>
2022-05-12 16:37:18 -0300

    renderizando html

---
+++ 3_core_modules/2_retornando_html/index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-type', 'text/html')
+    res.end('<h1>Olá este é o meu primeiro server com HTML!</h1>')
+})
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
