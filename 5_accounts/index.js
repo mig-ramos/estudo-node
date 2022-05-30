af1770936da14351522e9461e972b753f6da9fab
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-05-29 23:34:19 -0300

    Criado o menu de opcoes

--- 5_accounts/index.js
+++ 5_accounts/index.js
@@ -5,4 +5,29 @@ const chalk = require('chalk')
 // modulos internos
 const fs = require('fs')
 
-console.log('Iniciamos o Accounts')
+operation()
+
+function operation() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que deseja fazer?',
+        choices: [
+            'Criar conta',
+            'Consultar saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ],
+    },
+])
+.then((answer) => {
+
+const action = answer['action']
+
+console.log(action)
+
+})
+.catch((err) => console.log(err))
+    
+}
