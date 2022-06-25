f066a07f757c8b36fe7692b2c0f0b59a185978aa
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-06-25 20:14:51 -0300

    enviando parâmetros ao servidor

--- 6_express/2_setup/index.js
+++ 6_express/2_setup/index.js
@@ -7,6 +7,21 @@ const path = require('path')
 
 const basePath = path.join(__dirname, 'templates')
 
+// middleware
+const checkAuth = function(req, res, next){
+
+    req.authStatus =true
+
+    if(req.authStatus){
+        console.log('Está logado, pode continuar')
+        next()
+    } else {
+        console.log('Não está logado, faça o login para continuar')
+    }
+}
+
+app.use(checkAuth)
+
 app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`)
 })
