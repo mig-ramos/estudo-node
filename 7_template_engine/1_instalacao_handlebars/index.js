80a73a9a94f2cc808943dda35a51c87afa9c785e
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-06-26 22:06:28 -0300

    condicional else

--- 7_template_engine/1_instalacao_handlebars/index.js
+++ 7_template_engine/1_instalacao_handlebars/index.js
@@ -19,9 +19,10 @@ const user = {
 const palavra = 'Teste'
 
 const auth = true
+const approved = true
 
 app.get('/', (req, res) => {
-    res.render('home',{ user: user, palavra, auth })
+    res.render('home',{ user: user, palavra, auth, approved })
 })
 
 app.listen(3000, () => {
