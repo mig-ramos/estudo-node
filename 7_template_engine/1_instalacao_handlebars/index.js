9d38f92b9b59127594e25862e11541d1be0c205e
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-06-26 22:56:19 -0300

    mostrando propriedades com with

--- 7_template_engine/1_instalacao_handlebars/index.js
+++ 7_template_engine/1_instalacao_handlebars/index.js
@@ -12,6 +12,16 @@ app.get('/dashboard', (req, res) => {
     res.render('dashboard', {items})
 })
 
+app.get('/post', (req, res) => {
+    const post = {
+        title: 'Aprender Node JS',
+        category: 'JavaScript',
+        body: 'Este arqtigo vai te ajudar a aprender Node.js...',
+        comments: 4
+    }
+    res.render('blogpost', { post })
+})
+
 // exemplos de dados
 const user = {
     name: "Miguel",
