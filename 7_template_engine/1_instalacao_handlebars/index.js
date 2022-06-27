bfaac742761958c3bd8b0462e2a6130f105ead6f
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-06-26 23:38:27 -0300

    aplicando css

--- 7_template_engine/1_instalacao_handlebars/index.js
+++ 7_template_engine/1_instalacao_handlebars/index.js
@@ -10,6 +10,8 @@ const hbs = exphbs.create({
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+
 app.get('/dashboard', (req, res) => {
 
     const items = ["item a", "item b", "item c"]
