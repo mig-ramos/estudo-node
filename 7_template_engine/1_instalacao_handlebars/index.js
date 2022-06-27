f2e234f1109f8dbfbaef28f50bd7f82f44b1d7cf
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-06-26 21:30:55 -0300

    enviando variaveis para o body

--- 7_template_engine/1_instalacao_handlebars/index.js
+++ 7_template_engine/1_instalacao_handlebars/index.js
@@ -6,8 +6,15 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+const user = {
+    name: "Miguel",
+    surname: "Ramos"
+}
+
+const palavra = 'Teste'
+
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home',{ user: user, palavra })
 })
 
 app.listen(3000, () => {
