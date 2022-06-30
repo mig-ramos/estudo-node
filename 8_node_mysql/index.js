0ed0abf5e844936e6d333ad58f8c9f027f3ed963
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-06-30 18:57:39 -0300

    edicao - capturar os dados para edicao

--- 8_node_mysql/index.js
+++ 8_node_mysql/index.js
@@ -73,6 +73,22 @@ app.get('/books/:id', (req, res) => {
 })
 })
 
+app.get('/books/edit/:id', (req, res) => {
+
+    const id = req.params.id
+    const sql = `SELECT * FROM books WHERE id = ${id}`
+    conn.query(sql, function(err, data){
+        if(err){
+            console.log(err)
+            return
+        }
+    
+    const book = data[0]
+
+    res.render('editbook', { book })
+    })
+})
+
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
