1caecc11b967dfbf267f91714ec660b9795f0469
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-07-02 20:01:42 -0300

    conexao com sequelize

---
+++ 11_sequelize_orm/db/conn.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodesequelize', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+try {
+
+    sequelize.authenticate()
+    console.log('Conectamos com sucesso com o Sequelize!')
+    
+} catch (error) {
+    console.log('Não foi possível conectar: ', error)
+}
+
+module.exports = sequelize
