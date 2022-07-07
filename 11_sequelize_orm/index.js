96b70011a39d108af7110bf1350f863ccb9c7285
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-07-07 12:48:00 -0300

    buscando um  dado pelo sequelize

--- 11_sequelize_orm/index.js
+++ 11_sequelize_orm/index.js
@@ -43,6 +43,14 @@ app.post('/users/create', async (req, res) => {
     res.redirect('/')
 })
 
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id
+
+    const user =  await User.findOne({ raw: true, where:{ id: id }})
+
+    res.render('userview', { user })
+})
+
 app.get('/', async (req, res) => {
     
     // O raw age como um filtro deixando apenas os dados num array
