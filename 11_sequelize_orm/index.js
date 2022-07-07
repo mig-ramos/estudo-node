6aec638c33ae48a17fece764812a6a0f0dafb252
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-07-07 11:09:13 -0300

    inserindo dados pelo sequelize

--- 11_sequelize_orm/index.js
+++ 11_sequelize_orm/index.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 
 const user = require('./models/User')
 const conn = require('./db/conn')
+const User = require('./models/User')
 
 const app = express()
 
@@ -20,11 +21,33 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+app.get('/users/create' , (req, res) => {
+    res.render('adduser')
+})
+
+app.post('/users/create', async (req, res) => {
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if(newsletter === 'on'){
+        newsletter = true
+    } else {
+        newsletter = false
+    }
+
+    console.log(req.body)
+
+    await User.create({ name, occupation, newsletter })
+
+    res.redirect('/')
+})
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 conn.sync().then(() => {
-    app.listem(3000)
+    app.listen(3000)
 }).catch(err => console.log(err))
 
