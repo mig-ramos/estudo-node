8dbb606946ea1f35a33e0aef62dececedf4b31b4
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-07-10 23:22:59 -0300

    deletando enderecos

--- 11_sequelize_orm/index.js
+++ 11_sequelize_orm/index.js
@@ -127,6 +127,18 @@ app.post('/address/create', async (req, res) => {
     res.redirect(`/users/edit/${UserId}`)
 })
 
+
+app.post('/address/delete', async (req, res) => {
+    const UserId = req.body.UserId
+    const id = req.body.id
+    await Address.destroy({
+        where: {id: id},
+    })
+
+    res.redirect(`/users/edit/${UserId}`)
+})
+
+
 conn
 .sync()
 //.sync({force: true}) //FORÇAR RECRIAR AS TABELAS
