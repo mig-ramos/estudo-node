9df62b4f7891512185012efdc815b84e75ceaced
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-07-16 22:03:49 -0300

    resgatando dados e estilizando a página de todas tarefas

--- 12_mvc/routes/tasksRoutes.js
+++ 12_mvc/routes/tasksRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const TaskComtroller = require('../controllers/TaskController')
 
 router.get('/add', TaskComtroller.createTask)
+router.post('/add', TaskComtroller.createTaskSave)
 router.get('/', TaskComtroller.showTasks)
 
 module.exports = router
