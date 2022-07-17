dd7758347d8d98ae39370496b8f5a8279187eabf
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-07-17 01:11:13 -0300

    mudanca do status da tarefa

--- 12_mvc/controllers/TaskController.js
+++ 12_mvc/controllers/TaskController.js
@@ -52,6 +52,18 @@ module.exports = class TaskComtroller {
         res.redirect('/tasks')
     }
 
+    static async toggleTaskStatus(req,res) {
+
+        const id = req.body.id
+        const task = {
+            done: req.body.done === '0' ? true : false
+        }
+        
+        await Task.update(task, {where: {id: id}})
+
+        res.redirect('/tasks')
+    }
+
     static async showTasks(req, res) {
 
         const tasks = await Task.findAll({raw: true})
--- 12_mvc/routes/tasksRoutes.js
+++ 12_mvc/routes/tasksRoutes.js
@@ -8,6 +8,7 @@ router.post('/add', TaskComtroller.createTaskSave)
 router.post('/remove', TaskComtroller.removeTask)
 router.get('/edit/:id', TaskComtroller.updateTask)
 router.post('/edit', TaskComtroller.updateTaskSave)
+router.post('/updatestatus', TaskComtroller.toggleTaskStatus)
 router.get('/', TaskComtroller.showTasks)
 
 module.exports = router
